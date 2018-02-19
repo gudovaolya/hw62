32215096e85bf99064de0fd18a5c67c7ad0f4eb4
olga gudova <olya_0207@mail.ru>
2018-02-19 16:07:34 +0600

    добавлена карта на страницу контакты

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -5,13 +5,10 @@ class About extends Component {
     render () {
         return (
             <div className="inner-page container">
-                <div>
-                    <h2>About Us</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut nulla sapien, at aliquam erat. Sed vitae massa tellus. Aliquam commodo aliquam metus, sed iaculis nibh tempus id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam nec nisi in nisl euismod fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut nulla sapien, at aliquam erat. Sed vitae massa tellus. Aliquam commodo aliquam metus, sed iaculis nibh tempus id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam nec nisi in nisl euismod fringilla.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut nulla sapien, at aliquam erat. Sed vitae massa tellus. Aliquam commodo aliquam metus, sed iaculis nibh tempus id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam nec nisi in nisl euismod fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut nulla sapien, at aliquam erat. Sed vitae massa tellus. Aliquam commodo aliquam metus, sed iaculis nibh tempus id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam nec nisi in nisl euismod fringilla.</p>
-                </div>
+                <h2>About Us</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut nulla sapien, at aliquam erat. Sed vitae massa tellus. Aliquam commodo aliquam metus, sed iaculis nibh tempus id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam nec nisi in nisl euismod fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut nulla sapien, at aliquam erat. Sed vitae massa tellus. Aliquam commodo aliquam metus, sed iaculis nibh tempus id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam nec nisi in nisl euismod fringilla.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut nulla sapien, at aliquam erat. Sed vitae massa tellus. Aliquam commodo aliquam metus, sed iaculis nibh tempus id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam nec nisi in nisl euismod fringilla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut nulla sapien, at aliquam erat. Sed vitae massa tellus. Aliquam commodo aliquam metus, sed iaculis nibh tempus id. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam nec nisi in nisl euismod fringilla.</p>
                 <Services />
-
             </div>
         )
     }
--- src/containers/Contacts/Contacts.js
+++ src/containers/Contacts/Contacts.js
@@ -1,10 +1,36 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import './Contact.css';
+import GoogleMap from 'google-map-react';
+
+const Marker = ({ text }) => <div className="map-marker"><i className="fas fa-map-marker"></i><span className="marker-txt">{text}</span></div>;
 
 
 class Contacts extends Component {
+
     render () {
         return (
-            <h1>Contacts</h1>
+            <Fragment>
+                <div className="inner-page container">
+                    <h2>Contacts</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias commodi illo illum, ipsum nulla optio similique voluptas! A accusantium, earum eligendi ipsa mollitia nam odio pariatur placeat, porro quos tempore!</p>
+                </div>
+                <div className="map">
+                    <GoogleMap
+                        bootstrapURLKeys={{ key: 'AIzaSyB-11RybNOVzCor-S2TSWJkhHGDDSs_iis'}}
+                        defaultCenter={{lat: 42.84615529, lng: 74.59717283}}
+                        defaultZoom={11}
+                    >
+                        <Marker
+                            lat={42.84615529}
+                            lng={74.59717283}
+                            text={'г. Бишкек, ул. Молдыбаева, 7'}
+                        />
+                    </GoogleMap>
+                </div>
+
+            </Fragment>
+
+
         )
     }
 }
